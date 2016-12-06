38d91b0f0b590f75e2b45c7cccd3411b7c574adf
xsenechal <xsenechal@gmail.com>
2016-12-06 18:45:19 +0100

    moves translation as dictionary

--- data/moves-translation-en-fr.scrapper.js
+++ data/moves-translation-en-fr.scrapper.js
@@ -1,16 +1,14 @@
 //https://pgodb.net/fr/moves/
 var rows = $("tr[role=row]")
 
-var _moves = [];
+var _moves = {};
 
 $.each(rows, function(i,row){
   //var row = rows[0]
   var fr = $(row).find("td a div:first").text()
   var en = $(row).find("td a div[lang=en]").text()
   if(en && fr){
-    var move = {};
-    move[en]= fr;
-    _moves.push(move);
+    _moves[en]= fr;
   }
 });
 console.log(JSON.stringify(_moves));
